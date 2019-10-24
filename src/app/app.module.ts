098538151844512eca686c867af492884717bffb
Amardeep Yadav <amaryadav344@gmail.com>
2019-10-23 21:27:45 -0700

    Added header changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {TabModule} from 'angular-tabs-component';
 import {
   AutoCompleteModule,
   CheckboxModule,
+  ConfirmDialogModule,
   ContextMenuModule,
   DragDropModule,
   DropdownModule,
@@ -137,7 +138,8 @@ import {NewEntityDialogComponent} from './components/dialogs/new-entity-dialog/n
     DragDropModule,
     ToolbarModule,
     TabMenuModule,
-    OverlayPanelModule
+    OverlayPanelModule,
+    ConfirmDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent],
