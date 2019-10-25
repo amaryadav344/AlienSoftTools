7c94027d8631cadaa6883191c32507cddc77d73f
Amardeep Yadav <amaryadav344@gmail.com>
2019-10-25 06:11:20 -0700

    Added Herder popup changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,5 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {MenuItem} from 'primeng/api';
+import {Component, OnInit} from '@angular/core';
 import {IFile} from '../../models/IFile';
-import {OverlayPanel} from 'primeng/primeng';
 import {EntityService} from '../../services/entity-service/entity.service';
 import {WindowService} from '../../services/window/window.service';
 
@@ -11,10 +9,7 @@ import {WindowService} from '../../services/window/window.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  items1: MenuItem[];
   files: IFile[] = [];
-  @ViewChild('opEntity', {static: false})
-  opEntity: OverlayPanel;
   selection: IFile = {name: '', type: 0, path: ''};
   EntityPopup = false;
 
@@ -29,17 +24,6 @@ export class HeaderComponent implements OnInit {
         console.log(err);
       }
     );
-    this.items1 = [
-      {label: 'Stats', icon: 'pi pi-sitemap', command: this.openEntityDropdown},
-      {label: 'Calendar', icon: 'fa fa-fw fa-calendar'},
-      {label: 'Documentation', icon: 'fa fa-fw fa-book'},
-      {label: 'Support', icon: 'fa fa-fw fa-support'},
-      {label: 'Social', icon: 'fa fa-fw fa-twitter'}
-    ];
-  }
-
-  openEntityDropdown(event) {
-    this.opEntity.toggle(event);
   }
 
   loadMoreFiles(event) {
