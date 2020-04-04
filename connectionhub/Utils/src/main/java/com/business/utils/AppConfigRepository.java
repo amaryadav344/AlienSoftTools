ea7179d5acb5c9dab6fe75b246177526cd3ac6dd
Amardeep Yadav <amaryadav344@gmail.com>
2020-04-04 04:22:53 -0700

    Moved code into subdirectory

---
+++ connectionhub/Utils/src/main/java/com/business/utils/AppConfigRepository.java
@@ -0,0 +1,35 @@
+package com.business.utils;
+
+import com.business.utils.models.AppConfig;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class AppConfigRepository {
+    public static String DATABASE_URL = "DatabaseUrl";
+    public static String DATABASE_USERNAME = "DatabaseUsername";
+    public static String DATABASE_PASSWORD = "DatabasePassword";
+    public static String DATABASE_DRIVER = "DatabaseDriver";
+    public static String BASE_DIRECTORY = "BaseDirectory";
+    public static String BIN_RELATIVE_PATH = "BinRelativePath";
+    public static String XML_RELATIVE_PATH = "XMLRelativePath";
+    public static String BUSINESS_MODELS_RELATIVE_PATH = "BusinessModelsRelativePath";
+    public static String PACKAGE_NAME = "PackageName";
+    private static AppConfigRepository mAppConfigRepository;
+    AppConfig appConfig;
+
+    private AppConfigRepository() throws IOException {
+        appConfig = new XmlMapper().readValue(new File("C:\\Users\\Amardeep Yadav\\IdeaProjects\\bin\\AppConfig.xml"), AppConfig.class);
+    }
+
+    public static AppConfigRepository getInstance() throws IOException {
+        if (mAppConfigRepository == null)
+            mAppConfigRepository = new AppConfigRepository();
+        return mAppConfigRepository;
+    }
+
+    public String getAppConfig(String ConfigName) {
+        return appConfig.getConfigs().stream().filter(config -> config.getName().equals(ConfigName)).findFirst().get().getValue();
+    }
+}
---
+++ connectionhub/Utils/src/main/java/com/business/utils/models/UI/IGridLayout.java
@@ -0,0 +1,53 @@
+package com.business.utils.models.UI;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+public class IGridLayout extends IView {
+    @JacksonXmlProperty(isAttribute = true)
+    String columns;
+    @JacksonXmlProperty(isAttribute = true)
+    String rows;
+    String type;
+    IView[] controls;
+
+    public IGridLayout() {
+    }
+
+
+    public String getColumns() {
+        return columns;
+    }
+
+    public void setColumns(String columns) {
+        this.columns = columns;
+    }
+
+    public String getRows() {
+        return rows;
+    }
+
+    public void setRows(String rows) {
+        this.rows = rows;
+    }
+
+    @JsonIgnore()
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public IView[] getControls() {
+        return controls;
+    }
+
+    public void setControls(IView[] controls) {
+        this.controls = controls;
+    }
+}
